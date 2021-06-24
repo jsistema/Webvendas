fe910f5d3b2508692775c1ab541da5c099dcab95
João Paulo Magalhães <j.sistemas@hotmail.com>
2021-06-24 10:34:14 -0300

    Versão 1.0.0.4

---
+++ Webvendas/Data/WebvendasContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Webvendas.Models;
+
+namespace Webvendas.Data
+{
+    public class WebvendasContext : DbContext
+    {
+        public WebvendasContext (DbContextOptions<WebvendasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Webvendas.Models.Department> Department { get; set; }
+    }
+}
